b9aeb8632582029a67accda9c061aaee7594311a
Jesper Kjærgaard <jezper@hotmail.dk>
2024-03-18 09:44:04 +0100

    add responivness for our creation

--- src/App.tsx
+++ src/App.tsx
@@ -90,13 +90,16 @@ function App() {
 
       <main className="contain-width new-section d-flex flex-column">
         {/* Interactive VR */}
-        <div className="interactive-vr text-center">
-          <img
-            src="../images/mobile/image-interactive.jpg"
-            alt="Person interactive in vr"
-          />
-          <div className="mx-2">
-            <h3 className="mt-5 mb-4">The leader in interactive VR</h3>
+        <div className="interactive-vr row">
+          <div className="col-lg-8 col-12 interactive-vr__img">
+            <img
+              src="../images/mobile/image-interactive.jpg"
+              alt="Person interactive in vr"
+              ps-5
+            />
+          </div>
+          <div className="mx-2 text-center text-lg-start interactive-vr__info">
+            <h3 className="mt-5 mb-4 m-lg-0">The leader in interactive VR</h3>
             <p>
               Founded in 2011, Loopstudios has been producing world-class
               virtual reality projects for some of the best companies around the
@@ -107,13 +110,17 @@ function App() {
         </div>
 
         {/* Our creations */}
+        <div className="our-creation mt-5">
 
-        <div className="test mt-5">
-          <h3 className="text-center mb-5">Our creations</h3>
+          <div className="mb-5 d-flex justify-content-lg-between justify-content-center align-items-center">
+            <h3 className="text-center ">Our creations</h3>
+            <button className="d-none d-lg-block m-0">See all</button>
+          </div>
+
           <div className="row g-4">
             {mobilCreatonImages.map((img, index) => (
-              <div className="col-16 col-lg-3 hovereffectcard">
-                <div className="test__secound">
+              <div className="col-16 col-lg-3">
+                <div className="our-creation__secound">
                   <img
                     className="d-block d-lg-none"
                     src={`../images/mobile/${img}`}
@@ -125,22 +132,22 @@ function App() {
                     alt={`${img}`}
                   />
                   <h4>
-                    {mobilCreatonText[index].first} <br />{" "}
+                    {mobilCreatonText[index].first} <br />
                     {mobilCreatonText[index].second}
                   </h4>
                 </div>
               </div>
             ))}
           </div>
-          <div className="d-flex justify-content-center">
+
+          <div className="d-flex justify-content-center d-lg-none">
             <button>See all</button>
           </div>
         </div>
-
       </main>
 
       <footer className="text-center text-lg-start py-5">
-        <div className="contain-width d-flex flex-column flex-lg-row gap-5 justify-content-between">
+        <div className="contain-width d-flex flex-column flex-lg-row gap-3 justify-content-between">
           <div className="d-flex flex-column gap-4">
             <h4 className="logo">loopstudios</h4>
 
@@ -167,22 +174,30 @@ function App() {
 
           <div className="d-flex flex-column gap-4">
             <div className="d-flex justify-content-center gap-3 justify-content-lg-end">
-              <img
-                src="../images/icon-facebook.svg"
-                alt="Link to loopstuios Facebook page"
-              />
-              <img
-                src="../images/icon-twitter.svg"
-                alt="Link to loopstuios Twitter page"
-              />
-              <img
-                src="../images/icon-pinterest.svg"
-                alt="Link to loopstuios Pinterest page"
-              />
-              <img
-                src="../images/icon-instagram.svg"
-                alt="Link to loopstuios instergram page"
-              />
+              <a className="social-links" href="#">
+                <img
+                  src="../images/icon-facebook.svg"
+                  alt="Link to loopstuios Facebook page"
+                />
+              </a>
+              <a className="social-links" href="#">
+                <img
+                  src="../images/icon-twitter.svg"
+                  alt="Link to loopstuios Twitter page"
+                />
+              </a>
+              <a className="social-links" href="#">
+                <img
+                  src="../images/icon-pinterest.svg"
+                  alt="Link to loopstuios Pinterest page"
+                />
+              </a>
+              <a className="social-links" href="#">
+                <img
+                  src="../images/icon-instagram.svg"
+                  alt="Link to loopstuios instergram page"
+                />
+              </a>
             </div>
             <p>© 2021 Loopstudios. All rights reserved.</p>
           </div>
